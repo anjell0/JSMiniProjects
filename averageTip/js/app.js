be8bc3e50d3271c93c21b9e37698a9f5eae5deb5
anjell0 <aka2@iu.edu>
2022-06-26 15:20:26 -0400

    Updated tipper

--- averageTip/js/app.js
+++ averageTip/js/app.js
@@ -4,12 +4,22 @@
 
 let txtTipper = document.getElementById("txtTipper")            //Line 5 gets the element "txtTipper" from the HTML
 
-function tip() {                                                //Lines 7-15 create a function "tip" that will take
+function tip() {                                                //Lines 7-25 create a function "tip" that will take
     let billTotal = Number(txtTipper.value);                    //(only) a number input and multiply it by .18. It
-    let tipAmount = Math.round((billTotal * .18) * 100) / 100;  //will then log out a "Tip" amount and an overall
-                                                                //"Total" when "Calculate" is pressed. The text box
-    console.log("Tip: $" + tipAmount)                           //is then emptied
-    console.log("Total: $" + (tipAmount + billTotal))
+    let tipAmount = Math.round((billTotal * .18) * 100) / 100;  //will then display a "Tip" amount and an overall
+    let tip = document.createElement('div')                     //"Total" when "Calculate" is pressed. The text box
+    
+    tip.innerHTML = "Tip: $" + tipAmount
+    tip.style.fontSize = "30px"
+
+    document.body.appendChild(tip)
+
+    let total = document.createElement('div')
+
+    total.innerHTML = "Total: $" + (tipAmount + billTotal)
+    total.style.fontSize = "30px"
+
+    document.body.appendChild(total)
 
     txtTipper.value = "";
 }
